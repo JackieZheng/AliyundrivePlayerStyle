d8e424144f9e894b4032d06afe175b65cefa32fa
JackieZheng <zhengxinzhe@admans.net>
2021-10-29 11:47:49 +0800

    Update AliyundrivePlayerStyle.user.js

--- AliyundrivePlayerStyle.user.js
+++ AliyundrivePlayerStyle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AliyundrivePlayerStyle
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Aliyundrive Player Style
 // @author       JackieZheng
 // @match        *://www.aliyundrive.com/drive/folder/*
@@ -11,7 +11,7 @@
 
 GM_addStyle(".video-previewer,.video-previewer--1vo5c,.header,.header--2Vw8Y{padding: 10px !important;background: #000000;}");
 GM_addStyle(".video-player,.video-player--29_72{bottom: 0px !important;left: unset !important; -webkit-transform: unset !important;  transform: unset !important;}");
-GM_addStyle(".icon,.icon--d-ejA,.icon--d-ejA[data-render-as=svg] svg,.nav-text,.nav-text--2o_Eh,.filename,.filename--zkxdz,.nav-actions--3xj7P{fill:#aaa !important;color:#aaa;}");
+GM_addStyle(".icon,.icon--d-ejA,.icon--d-ejA[data-render-as=svg] svg,.nav-text,.nav-text--2o_Eh,.filename,.filename--zkxdz,.nav-actions--3xj7P{fill:#aaaaaa61 !important;color:#aaaaaa61;}");
 GM_addStyle(".toolbar-wrapper[data-visible=true],.toolbar-wrapper--1NDow[data-visible=true] {opacity: .4 !important;}");
 GM_addStyle(".video-player.show,.video-player--29_72.show--Zy5bU {opacity: .4 !important;}");
 
