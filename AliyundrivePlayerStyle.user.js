4eb7b2dc4897bae992def23647bdfd180bfd9263
JackieZheng <zhengxinzhe@admans.net>
2022-01-04 12:51:12 +0800

    Update AliyundrivePlayerStyle.user.js

--- AliyundrivePlayerStyle.user.js
+++ AliyundrivePlayerStyle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AliyundrivePlayerStyle
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.7
 // @description  Aliyundrive Player Style
 // @author       JackieZheng
 // @match        *://www.aliyundrive.com/drive/folder/*
@@ -12,11 +12,12 @@
 GM_addStyle(".video-previewer,.video-previewer--1vo5c,.header,.header--2Vw8Y{padding: 10px !important;background: #000000;}");
 GM_addStyle(".video-player,.video-player--29_72{bottom: 0px !important;left: unset !important; -webkit-transform: unset !important;  transform: unset !important;}");
 GM_addStyle(".icon,.icon--d-ejA,.icon--d-ejA[data-render-as=svg] svg,.nav-text,.nav-text--2o_Eh,.filename,.filename--zkxdz,.nav-actions--3xj7P{fill:#aaaaaa61 !important;color:#aaaaaa61;}");
-GM_addStyle(".toolbar-wrapper[data-visible=true],.toolbar-wrapper--1NDow[data-visible=true] {opacity: .4 !important;}");
-GM_addStyle(".video-player.show,.video-player--29_72.show--Zy5bU {opacity: .4 !important;}");
+GM_addStyle(".toolbar-wrapper[data-visible=true],.toolbar-wrapper--1NDow[data-visible=true] {opacity: .4 !important;height: 50px;overflow: hidden;}");
+GM_addStyle(".video-player.show,.video-player--29_72.show--Zy5bU {opacity: .4 !important;width: 100%;}");
+GM_addStyle(".toolbar-wrapper[data-visible=true]:hover,.toolbar-wrapper--1NDow[data-visible=true]:hover {opacity: 1 !important;height: auto;}");
 GM_addStyle("video{top:60px !important;height:calc( 100% - 60px ) !important;}");
 GM_addStyle(".video-stage.video,.video-stage.thumbnail,.video-stage--3LCB4.video--26SLZ,.video-stage--3LCB4.thumbnail--2T1JI{top:60px !important;height:calc( 100% - 60px ) !important;}");
-
+GM_addStyle(".drawer-item,.drawer-item--2cNtQ,.drawer-item[data-is-current=true],.drawer-item--2cNtQ[data-is-current=true]{justify-content: center !important;}");
 
 
 (function() {
