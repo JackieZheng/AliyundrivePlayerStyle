5378af89150cad2ede51b7d30b6990433d650243
JackieZheng <zhengxinzhe@admans.net>
2022-01-06 09:10:23 +0800

    Create AliyundrivePlayerStyle.user.js

--- AliyundrivePlayerStyle.user.js
+++ AliyundrivePlayerStyle.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         AliyundrivePlayerStyle
-// @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Aliyundrive Player Style
 // @author       JackieZheng
 // @match        *://www.aliyundrive.com/drive/*
 // @icon         https://gw.alicdn.com/imgextra/i3/O1CN01aj9rdD1GS0E8io11t_!!6000000000620-73-tps-16-16.ico
+// @namespace    https://greasyfork.org/en/scripts/434611
+// @supportURL   https://github.com/JackieZheng/AliyundrivePlayerStyle/issues
 // @grant    GM_addStyle
 // ==/UserScript==
 
